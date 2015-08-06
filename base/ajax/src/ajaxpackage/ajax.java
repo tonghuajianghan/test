c13a97c34be8616b089c7dd487e306f90e889aa5
tonghuajianghan <tonghuajianghan@gmail.com>
2015-08-06 16:12:08 +0800

    testgitmvn 15/8/6

--- base/ajax/src/ajaxpackage/ajax.java
+++ base/ajax/src/ajaxpackage/ajax.java
@@ -1,52 +1,57 @@
 package ajaxpackage;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 /**
  * Servlet implementation class ComeBack
  */
 public class ajax extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ajax() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public ajax() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");//接受过来的数据用uft——8表示
-		response.setContentType("text/html;charset=utf-8");//发送过来的数据用uft——8表示
-		PrintWriter out=response.getWriter();	//	
-		String name=request.getParameter("username");
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter(); //
+		String name = request.getParameter("username");
 		System.out.println(name);
-		String passward=request.getParameter("psw");
-		String okpassward=request.getParameter("confpsw");
-		if(name.equals("zhangsan")){
-			out.print("用户名已存在");//返回字符到jsp界面中由那边的
-		}else{
-			out.print("用户名可用");
+		String passward = request.getParameter("psw");
+		String okpassward = request.getParameter("confpsw");
+		if (name.equals("zhangsan")) {
+			out.print("ok name");
+		} else {
+			out.print("error name");
 		}
-		if(passward.equals(okpassward))
-		{
-			out.print("输入相同");
-		}else{
-			out.print("密码与确认密码输入不相同，请重新输入");
+		if (passward.equals(okpassward)) {
+			out.print("ok password");
+		} else {
+			out.print("error password");
 		}
-		
+
 	}
 
 }
--- base/ajax/src/ajaxpackage/ajaxresponse.java
+++ base/ajax/src/ajaxpackage/ajaxresponse.java
@@ -1,42 +1,46 @@
 package ajaxpackage;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-/*
- * ajax应用
- * ajax传输数据servlet接收数据
- * servlet回传给ajax数据
- * ajax负责显示
+
+/**
+ * @author jh
+ * @date 2015-8-5
+ * @Description: test ajax js 
  */
 public class ajaxresponse extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    public ajaxresponse() {
-        super();    
-    }	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
-		request.setCharacterEncoding("utf-8");//接受过来的数据用uft——8表示
-		response.setContentType("text/html;charset=utf-8");//发送过来的数据用uft——8表示
-		
-		PrintWriter out=response.getWriter();	//	
-		
-		String name=request.getParameter("name");		
-		String passward=request.getParameter("ps");
-		String okpassward=request.getParameter("cps");
-		if(name.equals("zhangsan")){
-			out.print("<p>用户名已存在</p>");//返回字符到jsp界面中由那边的
-		}else{						
-			out.print("<p>用户名可用</p>");
+
+	public ajaxresponse() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+
+		PrintWriter out = response.getWriter(); 
+
+		String name = request.getParameter("name");
+		System.out.println("name:" + name);
+		String passward = request.getParameter("ps");
+		String okpassward = request.getParameter("cps");
+		if (name.equals("zhangsan")) {
+			out.print("<p>hello ajax</p>");
+		} else {
+			out.print("<p>error zhangsan</p>");
 		}
-		if(passward.equals(okpassward))
-		{
-			out.print("<p>输入相同</p>");
-		}else{
-			out.print("<p>密码与确认密码输入不相同，请重新输入</p>");
+		if (passward.equals(okpassward)) {
+			out.print("<p>password OK</p>");
+		} else {
+			out.print("<p>password error</p>");
 		}
-		
+
 	}
 
 }
