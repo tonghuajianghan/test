64591521fb61b68af191261f9bc465157d55490f
jh <tonghuajianghan@gmail.com>
2016-04-18 21:05:19 +0800

    mybatis quick start 测试安装mybatis 的安装

---
+++ framework/mybatis/mybatis002/src/main/java/jh/mybatis002/App.java
@@ -0,0 +1,13 @@
+package jh.mybatis002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
